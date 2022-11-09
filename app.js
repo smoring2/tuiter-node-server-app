9ca84472adf4fa99c4ceb5f1b45ec4e1be28b6b9
smoring2 <smoring1995@gmail.com>
2022-11-09 12:56:50 -0800

    app js

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ import UserController
     from "./controllers/users/users-controller.js";
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 HelloController(app);
 UserController(app);
 TuitsController(app);
-app.listen(process.env.port || 4000);
+app.listen(process.env.PORT || 4000);
